c5ce345498f553ff05fb316b77fbd571fa87f26b
Stav Raviv <stav.raviv@eluv.io>
2025-05-15 11:06:32 -0700

    Fix weird backstack states during development

--- src/components/GoBackConfiguration.tsx
+++ src/components/GoBackConfiguration.tsx
@@ -1,35 +1,29 @@
-import { useCallback, useEffect } from 'react';
-import { BackHandler } from 'react-native';
+import { useCallback } from 'react';
 import { SupportedKeys } from "@/remote-control/SupportedKeys";
 import { useKey } from "@/hookes/useKey";
-import { useNavigation } from "expo-router";
+import { useNavigation, useRouter } from "expo-router";
 
 export const GoBackConfiguration = () => {
   const navigation = useNavigation();
-
-  useEffect(() => {
-    const event = BackHandler.addEventListener('hardwareBackPress', () => {
-      return true;
-    });
-
-    return () => {
-      event.remove();
-    };
-  }, []);
+  const router = useRouter();
 
   const goBackOnBackPress = useCallback(
     (pressedKey: SupportedKeys) => {
-      if (!navigation.isFocused) {
+      if (!navigation.isFocused()) {
         return false;
       }
       if (pressedKey !== SupportedKeys.Back) return false;
       if (navigation.canGoBack()) {
         navigation.goBack();
         return true;
+      } else {
+        // During development, the router can get in a state where the backstack is empty, but we aren't on "/".
+        // To avoid this case, we just navigate to "/" directly. This is a no-op if we are already on "/".
+        router.dismissTo("/");
       }
       return false;
     },
-    [navigation],
+    [navigation, router],
   );
 
   useKey(SupportedKeys.Back, goBackOnBackPress);
