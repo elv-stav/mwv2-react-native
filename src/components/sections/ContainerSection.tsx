e97b0dfeeff68ee058c23122fd32d6058ad00e0c
Stav Raviv <stav.raviv@eluv.io>
2025-05-15 15:21:45 -0700

    fix missing permissionContext in SectionContainers

--- src/components/sections/ContainerSection.tsx
+++ src/components/sections/ContainerSection.tsx
@@ -2,10 +2,10 @@ import { observer } from "mobx-react-lite";
 import { SectionComponentProps } from "@/components/sections/SectionComponentProps";
 import CarouselSection from "@/components/sections/CarouselSection";
 
-const ContainerSection = observer(({ section }: SectionComponentProps) => {
+const ContainerSection = observer(({ section, context }: SectionComponentProps) => {
   return (<>
     {section.sections_resolved?.map((subsection) => {
-      return <CarouselSection key={subsection.id} section={subsection} />;
+      return <CarouselSection key={subsection.id} section={subsection} context={context} />;
     })}
   </>);
 });
