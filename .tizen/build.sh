8d5532212687eef1c473915d9619c6ad3622fbc4
Stav Raviv <stav.raviv@eluv.io>
2025-05-19 15:32:20 -0700

    Add some tizen files and scripts

---
+++ .tizen/build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Don't run this script manually. Use NPM on the root project (e.g. 'npm run build-tizen').
+# Make sure the 'tizen' from Tizen Studio is in your PATH. (https://docs.tizen.org/application/tizen-studio/common-tools/command-line-interface/)
+
+# Export web app
+expo export -p web
+
+# Clean tizen build directories
+rm -rf .tizen/.build
+rm -rf .tizen/.buildSrc
+
+# Copy Tizen files
+cp -r .tizen/ .tizen/.buildSrc
+# Copy web-app files
+cp -r dist/ .tizen/.buildSrc
+
+# Tizen pre-package step. I'm not actually sure what this does besides copying all the contents to a new dir.
+tizen build-web -out ../.build/tizen-web-build -- ./.tizen/.buildSrc
+
+# Actually create a wgt package
+tizen package -t wgt -s Eluvio-Cert -- .tizen/.build/tizen-web-build -o .tizen/.build
---
+++ .tizen/install.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Don't run this script manually. Use NPM on the root project (e.g. 'npm run install-tizen').
+#bash ./.tizen/build.sh
+
+skipBuild=false
+
+while getopts ":s" option; do
+  case $option in
+    s) # Skip build
+      skipBuild=true
+      ;;
+    \?) # Invalid option
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+  esac
+done
+# remove the options from the positional parameters
+shift $(( OPTIND - 1 ))
+
+# Find existing package
+package=$(find .tizen/.build -name "*.wgt" | head -n 1)
+
+if [[ -n "$package" ]] && [[ $skipBuild ]]; then
+  echo "Skipping build step. Installing existing package $package"
+else
+  echo "Building before install"
+  bash ./.tizen/build.sh
+fi
+
+# Install the package
+tizen install -n "Eluvio Media Wallet.wgt" -- .tizen/.build
