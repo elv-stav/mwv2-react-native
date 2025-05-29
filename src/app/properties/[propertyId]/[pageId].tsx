0cf86f7820c2408e077a629d08311c01da748cd0
Stav Raviv <stav.raviv@eluv.io>
2025-05-29 14:05:44 -0700

    Support {property}/{page} routes (subproperty links)

---
+++ src/app/properties/[propertyId]/[pageId].tsx
@@ -0,0 +1,8 @@
+import PropertyDetail from "@/components/pages/PropertyDetail";
+import React from "react";
+
+const PropertySpecificPage = () => {
+  return <PropertyDetail />;
+};
+
+export default PropertySpecificPage;
