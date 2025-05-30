5875ea38155e6bf5e0fd9563b1c0153934d9fd41
Stav Raviv <stav.raviv@eluv.io>
2025-05-30 10:45:10 -0700

    Better singleton exports

--- src/data/Env.ts
+++ src/data/Env.ts
@@ -1,26 +1,24 @@
-const main = {
-  /* Main */
-  name: "main",
-  prettyName: "Main",
-  baseUrl: "https://main.net955305.contentfabric.io/s/main/",
-  configUrl: "https://main.net955305.contentfabric.io/config",
-  walletUrl: "https://wallet.contentfabric.io",
+class Main {
+  name = "main";
+  prettyName = "Main";
+  baseUrl = "https://main.net955305.contentfabric.io/s/main/";
+  configUrl = "https://main.net955305.contentfabric.io/config";
+  walletUrl = "https://wallet.contentfabric.io";
   // This doesn't have to be pre-generated.
   // once we fetch the config, this is just a base64 encoding of:
   // {"qspace_id": "${config.qspace.id}"}
-  staticToken: "eyJxc3BhY2VfaWQiOiAiaXNwYzJSVW9SZTllUjJ2MzNIQVJRVVZTcDFyWVh6dzEifQ==",
+  staticToken = "eyJxc3BhY2VfaWQiOiAiaXNwYzJSVW9SZTllUjJ2MzNIQVJRVVZTcDFyWVh6dzEifQ==";
 }
 
-const demo = {
-  name: "demov3",
-  prettyName: "Demo",
-  baseUrl: "https://demov3.net955210.contentfabric.io/a/demov3/",
-  configUrl: "https://demov3.net955210.contentfabric.io/config",
-  walletUrl: "https://wallet.demov3.contentfabric.io",
-  staticToken: "eyJxc3BhY2VfaWQiOiAiaXNwYzNBTm9WU3pOQTNQNnQ3YWJMUjY5aG81WVBQWlUifQ==",
+class Demo {
+  name = "demov3";
+  prettyName = "Demo";
+  baseUrl = "https://demov3.net955210.contentfabric.io/a/demov3/";
+  configUrl = "https://demov3.net955210.contentfabric.io/config";
+  walletUrl = "https://wallet.demov3.contentfabric.io";
+  staticToken = "eyJxc3BhY2VfaWQiOiAiaXNwYzNBTm9WU3pOQTNQNnQ3YWJMUjY5aG81WVBQWlUifQ==";
 }
 
-// The only reason we don't export the object directly is to not
-// allow code to reference "main" or "demo" directly.
-export default { ...main };
-// export default { ...demo };
+// Change export to set environment.
+export default new Main();
+// export default new Demo();
--- src/utils/Log.ts
+++ src/utils/Log.ts
@@ -6,51 +6,55 @@ enum Level {
   ERROR = "ERROR"
 }
 
-const Log = {
+class Log {
   v(message: string, data?: any) {
-    log(Level.VERBOSE, message, data);
-  },
-  d(message: string, data?: any) {
-    log(Level.DEBUG, message, data);
-  },
-  i(message: string, data?: any) {
-    log(Level.INFO, message, data);
-  },
-  w(message: string, data?: any) {
-    log(Level.WARN, message, data);
-  },
-  e(message: string, data?: any) {
-    log(Level.ERROR, message, data);
-  },
-};
-
-function log(level: Level, message: string, data?: any) {
-  let color;
-  switch (level) {
-    case Level.VERBOSE:
-      color = "#BBBBBB";
-      break;
-    case Level.DEBUG:
-      color = "#299999";
-      break;
-    case Level.INFO:
-      color = "#62C023";
-      break;
-    case Level.WARN:
-      color = "#BBB529";
-      break;
-    case Level.ERROR:
-      color = "#FF6B68";
-      break;
+    this.log(Level.VERBOSE, message, data);
   }
-  color = `color:${color}`;
-  message = `%c${message}`;
-  data = data || "";
-  if (level === Level.ERROR) {
-    console.error(message, color, data);
-  } else {
-    console.log(message, color, data);
+
+  d(message: string, data?: any) {
+    this.log(Level.DEBUG, message, data);
+  }
+
+  i(message: string, data?: any) {
+    this.log(Level.INFO, message, data);
+  }
+
+  w(message: string, data?: any) {
+    this.log(Level.WARN, message, data);
+  }
+
+  e(message: string, data?: any) {
+    this.log(Level.ERROR, message, data);
+  }
+
+  private log(level: Level, message: string, data?: any) {
+    let color;
+    switch (level) {
+      case Level.VERBOSE:
+        color = "#BBBBBB";
+        break;
+      case Level.DEBUG:
+        color = "#299999";
+        break;
+      case Level.INFO:
+        color = "#62C023";
+        break;
+      case Level.WARN:
+        color = "#BBB529";
+        break;
+      case Level.ERROR:
+        color = "#FF6B68";
+        break;
+    }
+    color = `color:${color}`;
+    message = `%c${message}`;
+    data = data || "";
+    if (level === Level.ERROR) {
+      console.error(message, color, data);
+    } else {
+      console.log(message, color, data);
+    }
   }
 }
 
-export default Log;
+export default new Log();
