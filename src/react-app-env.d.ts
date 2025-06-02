96b76f227dd9e40ad299a9acd7094cdfafe1a503
Stav Raviv <stav.raviv@eluv.io>
2025-06-02 12:13:33 -0700

    Add basic splash for slow devices

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -3,6 +3,11 @@ declare module '*.png' {
   export default value;
 }
 
+declare module '*.jpg' {
+  const value: import('react-native').ImageSourcePropType;
+  export default value;
+}
+
 declare module '*.svg' {
   const value: import('react-native').ImageSourcePropType;
   export default value;
