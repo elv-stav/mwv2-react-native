e16047fd4ffac62c4a9d7b144bce2f590569b320
Stav Raviv <stav.raviv@eluv.io>
2025-06-06 15:34:14 -0700

    use correct config reference

--- src/data/api/MediaWalletApi.ts
+++ src/data/api/MediaWalletApi.ts
@@ -52,7 +52,7 @@ export const MediaWalletApi = {
 
 async function request<R>(method: "GET" | "POST", path: string, resultParser: ZodType<R, any, any>, query: any = {}, body?: BodyInit) {
   return fabricConfigStore.promiseConfig().then(async config => {
-    const url = new URL(fabricConfigStore.config!.authdBaseUrl);
+    const url = new URL(config.authdBaseUrl);
     url.pathname += "/mw/" + path;
     Object.keys(query).forEach((key) => {
       url.searchParams.set(key, query[key]);
